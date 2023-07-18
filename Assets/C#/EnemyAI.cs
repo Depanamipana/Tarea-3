fc6600b73143ba40edffd56e169994bba200fa66
Depanamipana <kakoliin004@gmail.com>
2023-07-17 23:33:46 -0400

    aaaa

--- Assets/C#/EnemyAI.cs
+++ Assets/C#/EnemyAI.cs
@@ -7,6 +7,7 @@ public class EnemyAI : MonoBehaviour
     public Transform player;
     public float attackDistance = 1f;
     public float moveSpeed = 2f;
+    public float followDistance = 5f; // Distancia a la que el enemigo comenzará a seguir al jugador
 
     private Animator animator;
     private bool isAttacking = false;
@@ -20,16 +21,15 @@ public class EnemyAI : MonoBehaviour
 
     void Update()
     {
-        if (isAttacking) {
+        if (isAttacking)
+        {
             currentAttackTime += Time.deltaTime;
-            if(currentAttackTime > attackTime)
+            if (currentAttackTime > attackTime)
             {
                 isAttacking = false;
             }
             return;
-        
         }
-            
 
         float distanceToPlayer = Vector2.Distance(transform.position, player.position);
 
@@ -39,16 +39,19 @@ public class EnemyAI : MonoBehaviour
             currentAttackTime = 0;
             isAttacking = true;
         }
-        else
+        else if (distanceToPlayer <= followDistance) // Verificar si el jugador está dentro de la distancia de seguimiento
         {
             animator.SetBool("IsWalk", true);
             transform.position = Vector3.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
 
             if (player.position.x < transform.position.x)
-                
                 transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x) * -1, transform.localScale.y, transform.localScale.z);
             else
                 transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }
+        else
+        {
+            animator.SetBool("IsWalk", false);
         }
     }
 
